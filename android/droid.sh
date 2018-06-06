643afd38a84f89ebeeb783994a5b01de6c573a2e
Luiz Gustavo Pozzo <luiz.pozzo@travelportdigital.com>
2018-06-06 11:54:58 +0100

    starting a trending for in android tools

---
+++ android/droid.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# meant to be run inside an Android adb shell console
+# Created has probably created this script from a Lineage OS
+
+set -e
+
+if [[ -z $1 ]]; then
+        echo "say me what"
+        exit
+fi;
+
+function main() {
+        case $1 in
+		top)
+			top
+			;;
+                *)
+                        echo "Unknown command"
+                        ;;
+        esac
+}
+
+function top() {
+	watch -t -n 7 -e 'top | head -n 10'
+}
+
+main "$@"
+
